f07bb49808d2d6a694394897a6e25f2cf46add4b
ukon1990 <mail@jonaskf.net>
2017-01-06 22:13:16 +0100

    indenting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,68 +4,68 @@ import { user } from './utils/globals';
 import { IUser } from './utils/interfaces';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-	providers: [AuctionService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [AuctionService]
 })
 export class AppComponent {
-  // http://realfavicongenerator.net/
-  private title = 'WAH';
-  private lastModified: number;
-  private timeSinceLastModified: number;
-  private oldTimeDiff: number;
-  private date: Date;
+    // http://realfavicongenerator.net/
+    private title = 'WAH';
+    private lastModified: number;
+    private timeSinceLastModified: number;
+    private oldTimeDiff: number;
+    private date: Date;
 
-  u: IUser = new IUser();
+    u: IUser = new IUser();
 
-  constructor(private auctionService: AuctionService) { this.u = user; }
+    constructor(private auctionService: AuctionService) { this.u = user; }
 
-  ngOnInit() {
-    this.date = new Date();
-    if (this.isRealmSet()) {
-      this.u.region = localStorage.getItem('region');
-      this.u.realm = localStorage.getItem('realm');
-      this.u.character = localStorage.getItem('character');
-      this.checkForUpdate();
+    ngOnInit() {
+        this.date = new Date();
+        if (this.isRealmSet()) {
+            this.u.region = localStorage.getItem('region');
+            this.u.realm = localStorage.getItem('realm');
+            this.u.character = localStorage.getItem('character');
+            this.checkForUpdate();
+        }
+        setInterval(() => this.setTimeSinceLastModified(), 1000);
+        setInterval(() => this.checkForUpdate(), 60000);
     }
-    setInterval(() => this.setTimeSinceLastModified(), 1000);
-    setInterval(() => this.checkForUpdate(), 60000);
-  }
 
-  setTimeSinceLastModified() {
-    this.date = new Date();
-    let updateTime =  new Date(this.lastModified).getMinutes(),
-      currentTime = this.date.getMinutes(),
-      oldTime = this.timeSinceLastModified;
-      // Checking if there is a new update available
-    if(this.timeDiff(updateTime, currentTime) < this.oldTimeDiff) {
-      this.auctionService.getAuctions();
+    setTimeSinceLastModified() {
+        this.date = new Date();
+        let updateTime = new Date(this.lastModified).getMinutes(),
+            currentTime = this.date.getMinutes(),
+            oldTime = this.timeSinceLastModified;
+        // Checking if there is a new update available
+        if (this.timeDiff(updateTime, currentTime) < this.oldTimeDiff) {
+            this.auctionService.getAuctions();
+        }
+
+        this.timeSinceLastModified = this.timeDiff(updateTime, currentTime);
+        this.oldTimeDiff = this.timeDiff(updateTime, currentTime);
+
     }
-    
-    this.timeSinceLastModified = this.timeDiff(updateTime, currentTime);
-    this.oldTimeDiff = this.timeDiff(updateTime, currentTime);
-    
-  }
 
-  timeDiff(updateTime, currentTime) {
-    return (updateTime > currentTime ?
-        (60 - updateTime + currentTime) : currentTime - updateTime);
-  }
-
-  exists(value): boolean {
-    return value !== null && value !== undefined && value.length > 0;
-  }
-
-  isRealmSet(): boolean {
-    return this.exists(localStorage.getItem('realm')) &&
-      this.exists(localStorage.getItem('region'));
-  }
-
-  checkForUpdate() {
-    if(this.isRealmSet()) {
-      this.auctionService.getLastUpdated()
-        .subscribe(r => this.lastModified = r['lastModified']);
+    timeDiff(updateTime, currentTime) {
+        return (updateTime > currentTime ?
+            (60 - updateTime + currentTime) : currentTime - updateTime);
     }
-  }
+
+    exists(value): boolean {
+        return value !== null && value !== undefined && value.length > 0;
+    }
+
+    isRealmSet(): boolean {
+        return this.exists(localStorage.getItem('realm')) &&
+            this.exists(localStorage.getItem('region'));
+    }
+
+    checkForUpdate() {
+        if (this.isRealmSet()) {
+            this.auctionService.getLastUpdated()
+                .subscribe(r => this.lastModified = r['lastModified']);
+        }
+    }
 }
