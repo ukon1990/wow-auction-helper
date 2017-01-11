5e8eafaecbae56341da23ad3c9f2a0bd213d38e9
ukon1990 <mail@jonaskf.net>
2017-01-11 06:43:57 +0100

    tslint + linting

--- src/app/services/auctions.ts
+++ src/app/services/auctions.ts
@@ -6,22 +6,22 @@ import { user } from '../utils/globals';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class AuctionService{
-  private user;
-  constructor(private http: Http){
-    this.user = user;
-  }
+export class AuctionService {
+    private user;
+    constructor(private http: Http) {
+        this.user = user;
+    }
 
-  getAuctions(){
-      let localUrl: string = '/assets/auctions.json';
-      let apiUrl: string = 'http://www.wah.jonaskf.net/GetAuctions.php?region=' + this.user.region + '&realm=' + this.user.realm;
-    return this.http.get(localUrl)
-      .map(response => <IAuction>function(r){ console.log('Loaded auctions'); return r;  }(response.json()));
-  }
+    getAuctions() {
+		let localUrl: string = '/assets/auctions.json';
+		let apiUrl: string = 'http://www.wah.jonaskf.net/GetAuctions.php?region=' + this.user.region + '&realm=' + this.user.realm;
+		return this.http.get(apiUrl)
+			.map(response => <IAuction>function(r) { console.log('Loaded auctions'); return r; } (response.json()));
+	}
 
-  getLastUpdated(){
-    return this.http.get('http://www.wah.jonaskf.net/GetAuctions.php?region='
-      + this.user.region + '&realm=' + this.user.realm + '&lastModified')
-      .map(response => <IAuction>function(r){ console.log('Loaded auctions'); return r;  }(response.json()));
-  }
+	getLastUpdated() {
+		return this.http.get('http://www.wah.jonaskf.net/GetAuctions.php?region='
+			+ this.user.region + '&realm=' + this.user.realm + '&lastModified')
+			.map(response => <IAuction>function(r) { console.log('Loaded auctions'); return r; } (response.json()));
+	}
 }
