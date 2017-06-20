c27a462dbf6d874b5227d65c90b8ca4a7eeb620f
ukon1990 <mail@jonaskf.net>
2017-06-20 08:35:53 +0200

    Copper conversion pipe

---
+++ src/app/pipes/gold.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'gold'
+})
+export class GoldPipe implements PipeTransform {
+
+	transform(copper: number, args?: any): string {
+		let result = [];
+		copper = Math.round(copper);
+		result[0] = copper % 100;
+		copper = (copper - result[0]) / 100;
+		// Silver
+		result[1] = copper % 100;
+        	// Gold
+		result[2] = ((copper - result[1]) / 100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+		return result[2] + 'g ' + result[1] + 's ' + result[0] + 'c';
+	}
+}
