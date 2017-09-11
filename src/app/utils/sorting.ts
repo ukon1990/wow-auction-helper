39a2d170f9a2b1b7d215f9db80d74b544b1edad6
ukon1990 <mail@jonaskf.net>
2017-09-11 20:03:14 +0200

    started

---
+++ src/app/utils/sorting.ts
@@ -0,0 +1,28 @@
+export class Sorting {
+	keys: Key[];
+
+	addKey(key: string): void {
+		// Logic
+	}
+
+	sort(arr: any[]): void {
+		// Logic
+	}
+
+	removeKey(key: string): void {
+		// Logic
+	}
+
+	findKeyIndex(key: string): number {
+		return 1;
+	}
+}
+
+class Key {
+	key: string;
+	order: string;
+}
+
+enum Enums {
+	ASC, DESC
+}
