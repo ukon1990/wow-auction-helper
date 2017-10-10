7d1af7b742558755a6314c3413e1785e73bf39fa
ukon1990 <mail@jonaskf.net>
2017-10-10 22:45:53 +0200

    upgraded angular version + material input fields in crafting

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,35 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-	MdButtonModule, MdCheckboxModule,
-	MdProgressBarModule, MdInputModule,
-	MdAutocompleteModule, MdToolbarModule,
-	MdListModule, MdTabsModule,
-	MdSidenavModule, MdPaginatorModule,
-	MdSortModule, MdTableModule,
-	MdCardModule, MdProgressSpinnerModule,
-	MdSelectModule
+	MatButtonModule, MatCheckboxModule,
+	MatProgressBarModule, MatInputModule,
+	MatAutocompleteModule, MatToolbarModule,
+	MatListModule, MatTabsModule,
+	MatSidenavModule, MatPaginatorModule,
+	MatSortModule, MatTableModule,
+	MatCardModule, MatProgressSpinnerModule,
+	MatSelectModule
 } from '@angular/material';
 
 @NgModule({
 	exports: [
 		CommonModule,
 		BrowserAnimationsModule,
-		MdButtonModule,
-		MdCheckboxModule,
-		MdProgressBarModule,
-		MdInputModule,
-		MdAutocompleteModule,
-		MdToolbarModule,
-		MdListModule,
-		MdTabsModule,
-		MdSidenavModule,
-		MdPaginatorModule,
-		MdSortModule,
-		MdTableModule,
-		MdCardModule,
-		MdProgressSpinnerModule,
-		MdSelectModule
+		MatButtonModule,
+		MatCheckboxModule,
+		MatProgressBarModule,
+		MatInputModule,
+		MatAutocompleteModule,
+		MatToolbarModule,
+		MatListModule,
+		MatTabsModule,
+		MatSidenavModule,
+		MatPaginatorModule,
+		MatSortModule,
+		MatTableModule,
+		MatCardModule,
+		MatProgressSpinnerModule,
+		MatSelectModule
 	],
 	imports: [
 	],
