2a20fcb035b4933d0daae37837ec94282695ad17
ukon1990 <mail@jonaskf.net>
2017-11-09 21:28:45 +0100

    Further work on the dropdown

--- src/app/components/downloads/downloads.component.ts
+++ src/app/components/downloads/downloads.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-downloads',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadsComponent implements OnInit {
 
+	@Input() timeSinceLastModified: any;
 	showDropdown: boolean;
 	constructor() { }
 
