25fac7b5acc6cd08302c396ff3ac61f6f7f3d985
ukon1990 <mail@jonaskf.net>
2017-11-09 21:43:48 +0100

    Added DL functions to DL component

--- src/app/components/downloads/downloads.component.ts
+++ src/app/components/downloads/downloads.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Crafting from 'app/utils/crafting';
+import Pets from 'app/utils/pets';
+import { Item } from 'app/utils/item';
+import Auctions from 'app/utils/auctions';
+import { ItemService } from 'app/services/item.service';
+import { AuctionService } from 'app/services/auctions.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-downloads',
@@ -10,7 +17,7 @@ export class DownloadsComponent implements OnInit {
 	@Input() timeSinceLastModified: any;
 	pageLoadTimestamp: Date = new Date();
 	showDropdown: boolean;
-	constructor() { }
+	constructor(private itemService: ItemService, private auctionService: AuctionService, private router: Router) { }
 
 	ngOnInit() {
 	}
@@ -19,4 +26,23 @@ export class DownloadsComponent implements OnInit {
 		return localStorage[`timestamp_${type}`]
 	}
 
+	donloadRecipes(): void {
+		Crafting.download(this.itemService);
+	}
+
+	downloadPets(): void {
+		Pets.download(this.itemService);
+	}
+
+	downloadItems(): void {
+		Item.download(this.itemService);
+	}
+
+	checkForUpdates(): void {
+		Auctions.checkForUpdates(this.auctionService);
+	}
+
+	downloadAuctions(): void {
+		Auctions.download(this.auctionService, this.router);
+	}
 }
