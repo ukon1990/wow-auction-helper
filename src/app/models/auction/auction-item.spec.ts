45543311cbafd84503144242ab03ca610749e0d5
ukon1990 <mail@jonaskf.net>
2017-12-06 18:15:21 +0100

    started on some item organization

--- src/app/models/auction/auction-item.spec.ts
+++ src/app/models/auction/auction-item.spec.ts
@@ -5,14 +5,56 @@ import { AuctionItem } from './auction-item';
 
 beforeEach(() => {
   // SharedService.tsm[]
-  AuctionItem.organize([]);
+  AuctionItem.organize([
+    {
+      'auc': 261623576, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623579, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623571, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623562, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623564, 'item': 74248, 'owner': 'Hoodin', 'ownerRealm': 'Draenor',
+      'bid': 312085, 'buyout': 312085, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623566, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623552, 'item': 74248, 'owner': 'Hoodin', 'ownerRealm': 'Draenor',
+      'bid': 312085, 'buyout': 312085, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261099273, 'item': 32428, 'owner': 'Santadrood', 'ownerRealm': 'Draenor',
+      'bid': 8286, 'buyout': 9207, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623555, 'item': 74248, 'owner': 'Hoodin', 'ownerRealm': 'Draenor',
+      'bid': 312085, 'buyout': 312085, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }, {
+      'auc': 261623557, 'item': 151566, 'owner': 'Dahilla', 'ownerRealm': 'Draenor',
+      'bid': 279800, 'buyout': 279800, 'quantity': 1, 'timeLeft': 'VERY_LONG', 'rand': 0,
+      'seed': 0, 'context': 0
+    }
+  ]);
 });
 
 
 describe('Auctions', () => {
   describe('Organizing auctions', () => {
     it('Should be able to organize auctions', () => {
-      expect(SharedService.auctionItems[142335]).toBeTruthy();
+      expect(SharedService.auctionItems[151566]).toBeTruthy();
     });
   });
 });
--- src/app/models/auction/auction-item.ts
+++ src/app/models/auction/auction-item.ts
@@ -26,6 +26,27 @@ export class AuctionItem {
    * @param auctions A raw auction array
    */
   public static organize(auctions: Array<Auction>): void {
-    // SharedService.auction
+    // Sorting by buyout, before we do the grouping for less processing.
+    auctions.sort((a, b)  => {
+      return a.buyout - b.buyout;
+    });
+
+    SharedService.auctions = auctions;
+    auctions.forEach(a => {
+      if (!SharedService.auctionItems[a.item]) {
+        SharedService.auctionItems[a.item] = this.auctionToAuctionItem(a);
+      } else {
+        SharedService.auctionItems[a.item].quantityTotal += a.quantity;
+        SharedService.auctionItems[a.item].auctions.push(a);
+      }
+    });
+  }
+
+  private static auctionToAuctionItem(auction: Auction): AuctionItem {
+    const tmpAuc = new AuctionItem();
+    tmpAuc.buyout = auction.buyout;
+    tmpAuc.bid = auction.bid;
+    tmpAuc.quantityTotal += auction.quantity;
+    return tmpAuc;
   }
 }
