39ba8947ebb8df36f6253bab0f8a7d47fd8dd37d
ukon1990 <mail@jonaskf.net>
2018-03-04 22:55:40 +0100

    Character portraits should now appear correctly regardless of region

--- src/app/components/settings/characters/character/character.component.ts
+++ src/app/components/settings/characters/character/character.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Character } from '../../../../models/character/character';
+import { SharedService } from '../../../../services/shared.service';
 
 @Component({
   selector: 'wah-character',
@@ -14,12 +15,12 @@ export class CharacterComponent {
   @Output() update: EventEmitter<any> = new EventEmitter();
 
   getAvatar(img: string): string {// thumbnail
-    return 'url(https://render-eu.worldofwarcraft.com/character/' + this.character.thumbnail + ')';
+    return `url(https://render-${ SharedService.user.region }.worldofwarcraft.com/character/${ this.character.thumbnail })`;
   }
 
   getBackgroundImage() {
     if (this.character.thumbnail) {
-      const url = `https://render-eu.worldofwarcraft.com/character/${
+      const url = `https://render-${ SharedService.user.region }.worldofwarcraft.com/character/${
         this.character.thumbnail.replace('avatar', 'main')
         }`;
       return 'url(' + url + ')';
