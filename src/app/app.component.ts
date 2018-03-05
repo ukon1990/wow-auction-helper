e69471d08d70fc61d288f8f759836b5f2d6b9388
ukon1990 <mail@jonaskf.net>
2018-03-05 09:53:38 +0100

    GA event for standalone devices (so that I know if peopel use it or not)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from './models/user/user';
 import { SharedService } from './services/shared.service';
@@ -6,24 +6,35 @@ import { CraftingService } from './services/crafting.service';
 import { AuctionsService } from './services/auctions.service';
 import { ItemService } from './services/item.service';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+import { Angulartics2 } from 'angulartics2';
 
 @Component({
   selector: 'wah-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   constructor(private _router: Router,
     private _craftingService: CraftingService,
     private _auctionsService: AuctionsService,
     private _itemService: ItemService,
-    angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
+    private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
+    private angulartics2: Angulartics2) {
     User.restore();
     SharedService.user.shoppingCart.restore();
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    if (window.navigator['standalone'] || window.matchMedia('(display-mode: standalone)').matches) {
+      this.angulartics2.eventTrack.next({
+        action: 'Standalone startup',
+        properties: { category: `Standalone device: ${window.navigator.platform}, ${window.navigator.vendor}` },
+      });
+    }
   }
 
   /* istanbul ignore next */
