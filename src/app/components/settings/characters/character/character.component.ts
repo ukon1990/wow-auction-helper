69f490692b761153f9cab12a1323e8b9be691769
ukon1990 <mail@jonaskf.net>
2018-03-09 22:23:11 +0100

    Character backgrounds should work as expected now in the setup window

--- src/app/components/settings/characters/character/character.component.ts
+++ src/app/components/settings/characters/character/character.component.ts
@@ -10,21 +10,30 @@ import { SharedService } from '../../../../services/shared.service';
 export class CharacterComponent {
   @Input() index: number;
   @Input() character: Character;
+  @Input() region: string;
   @Input() isSeller: boolean;
   @Output() remove: EventEmitter<any> = new EventEmitter();
   @Output() update: EventEmitter<any> = new EventEmitter();
 
   getAvatar(img: string): string {// thumbnail
-    return `url(https://render-${ SharedService.user.region }.worldofwarcraft.com/character/${ this.character.thumbnail })`;
+    return `url(https://render-${
+      this.getRegion()
+    }.worldofwarcraft.com/character/${
+      this.character.thumbnail
+    })`;
   }
 
   getBackgroundImage() {
     if (this.character.thumbnail) {
-      const url = `https://render-${ SharedService.user.region }.worldofwarcraft.com/character/${
+      const url = `https://render-${ this.getRegion() }.worldofwarcraft.com/character/${
         this.character.thumbnail.replace('avatar', 'main')
         }`;
       return 'url(' + url + ')';
     }
     return '';
+  }
+
+  getRegion(): string {
+    return SharedService.user.region ? SharedService.user.region : this.region;
   }
 }
