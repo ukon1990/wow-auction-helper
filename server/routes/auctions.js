3f74521227d42c16cf2beb909d5e6472ed8b0670
ukon1990 <mail@jonaskf.net>
2018-03-12 21:53:37 +0100

    Added backend support for wowuction.
    Unsure if this will be supported in the app or not.

--- server/routes/auctions.js
+++ server/routes/auctions.js
@@ -1,6 +1,6 @@
 const express = require('express'),
   router = express.Router(),
-  headers  = require('./headers'),
+  headers = require('./headers'),
   url = require('url'),
   request = require('request'),
   secrets = require('../secrets/secrets'),
@@ -19,6 +19,44 @@ let response = {
   data: [],
   message: null
 };
+
+router.get('/wowuction/:region/:realm/:key', (req, res) => {
+  const key = req.query.key;
+  // http://localhost:3000/wuc.tsv
+  request.get(`http://www.wowuction.com/${
+        req.params.region
+      }/${
+        req.params.realm
+      }/alliance/Tools/RealmDataExportGetFileStatic?token=${
+        req.params.key
+      }`, (err, re, body) => {
+    res = headers.setHeaders(res);
+    const list = [];
+    let obj = {},
+      tempObj = {},
+      isFirst = true;
+    // 5 == itemID, 7 == market price,
+    // 14 == Avg Daily Posted, 15 == Avg Estimated Daily Sold,
+    // 16 == Estimated demand
+    body.split('\n').forEach(l => {
+      if (isFirst) {
+        isFirst = false;
+        // console.log(l.split('\t'));
+      } else {
+        tempObj = l.split('\t');
+        list.push({
+          id: parseInt(tempObj[4], 10),
+          mktPrice: parseInt(tempObj[6]),
+          avgDailyPosted: parseFloat(tempObj[15]),
+          avgDailySold: parseFloat(tempObj[16]),
+          estDemand: parseFloat(tempObj[17]),
+          dailyPriceChange: parseFloat(tempObj[14])
+        });
+      }
+    });
+    res.send(list);
+  });
+});
 
 router.get('*', (req, res) => {
   const url = req.query.url;
