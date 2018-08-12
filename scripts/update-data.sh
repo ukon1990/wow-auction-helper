eca2c65c334a0ef8c2ab662292e6dac5adeb9500
ukon1990 <mail@jonaskf.net>
2018-08-12 08:30:33 +0200

    Updated the dl script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -2,17 +2,22 @@
 # Items
 declare locales=('en_GB' 'de_DE' 'es_ES' 'fr_FR' 'it_IT' 'pl_PL' 'pt_PT' 'ru_RU' 'en_US' 'es_MX' 'pt_BR')
 rm items*.json
+rm items*.json.gz
 rm recipes*.json
+rm recipes*.json.gz
 rm pets*.json
+rm pets*.json.gz
 
 echo ${locales[@]}
 
 for i in ${locales[@]}; do
-    echo Downloading locale ${locales[$i]}
-    curl --header "Content-Type: application/json" --request POST --data '{}' http://localhost:3000/api/item?locale=${locales[$i]} --output items-${locales[$i]}.json
-    gzip items-${locales[$i]}.json
-    curl --header "Content-Type: application/json" --request POST --data '{}' http://localhost:3000/api/recipe?locale=${locales[$i]} --output recipes-${locales[$i]}.json
-    gzip recipes-${locales[$i]}.json
-    curl --header "Content-Type: application/json" --request POST --data '{}' http://localhost:3000/api/pets?locale=${locales[$i]} --output pets-${locales[$i]}.json
-    gzip pets-${locales[$i]}.json
+    echo Downloading locale $i
+    curl --header "Content-Type: application/json" --request POST --data '{"timestamp": "2000-06-30T00:00:00.000Z"}' 'http://localhost:3000/api/item?locale='$i'' --output 'items-'$i'.json'
+    gzip 'items-'$i'.json'
+    curl --header "Content-Type: application/json" --request POST --data '{"timestamp": "2000-06-30T00:00:00.000Z"}' 'http://localhost:3000/api/recipe?locale='$i'' --output 'recipes-'$i'.json'
+    gzip 'recipes-'$i'.json'
+    curl --header "Content-Type: application/json" --request POST --data '{"timestamp": "2000-06-30T00:00:00.000Z"}' 'http://localhost:3000/api/pets?locale='$i'' --output 'pets-'$i'.json'
+    gzip 'pets-'$i'.json'
 done    
+
+echo 'Done downloading data'
