8bc52c254df9f29b2993c04c426e4d8fba8a7892
ukon1990 <mail@jonaskf.net>
2018-08-22 17:59:47 +0200

    .

--- server-lambda/auction.ts
+++ server-lambda/auction.ts
@@ -4,50 +4,63 @@ const request: any = require('request');
 const RequestPromise = require('request-promise');
 
 exports.getAuctions = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  console.log('',event);
-  let url = event['url'] || JSON.parse(event.body).url;
+  console.log('', event);
+  const testing = false;
+  const url = testing ?
+    'http://localhost:4200/assets/mock/auctions.json' : event['url'] || JSON.parse(event.body).url;
+  const response = {
+    realms: [],
+    auctions: []
+  };
   try {
-    let response = {
-      realms: [],
-      auctions: []
-    };
-    if (url) {
+    if (url && testing) {
       console.log('url', url);
-  
-      if (url && url.indexOf('.worldofwarcraft.com/auction-data') !== -1) {
+      if (url && (url.indexOf('.worldofwarcraft.com/auction-data') !== -1 || testing)) {
         console.log('Contains url');
         request.get(url, (error, res, body) => {
+          console.log('inside');
           if (error) {
             console.error('getAuctions', error);
-            callback(null, response);
+            gzipResponse(context, JSON.stringify(response), callback);
             return;
           }
           console.log('attempting zlib');
-          zlib.gzip(new Buffer(body, 'utf-8'), function(error, ahData) {
-            if(error) context.fail(error);
-            console.log('zlibbed');
-            const res = {
-                statusCode: 200,
-                body: ahData.toString('base64'),
-                isBase64Encoded: true,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Encoding': 'gzip'
-                }
-            };
-            // context.succeed(response);
-            callback(null, res);
+          gzipResponse(context, body, callback);
         });
-        });
+      } else {
+        gzipResponse(context, response, callback);
       }
-  
     } else {
       console.log('else', event);
+      gzipResponse(context, response, callback);
     }
   } catch (error) {
     console.log(error);
-    callback(null, error);
+    gzipResponse(context, response, callback);
   }
+};
+
+function gzipResponse(context: Context, body, callback: Callback): void {
+  zlib.gzip(
+    new Buffer(
+      typeof body === 'string' ? body : JSON.stringify(body), 'utf-8'),
+    (err, ahData) => {
+    if (err) {
+      context.fail(err);
+    }
+    const res = {
+        statusCode: 200,
+        body: ahData.toString('base64'),
+        isBase64Encoded: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Encoding': 'gzip'
+        }
+    };
+    // context.succeed(response);
+    console.log('gzipped');
+    callback(null, res);
+  });
 }
 
 function removeUnused(auctionResponse) {
