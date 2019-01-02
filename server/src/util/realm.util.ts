a68cc78c32ebbff2b7e92114dfc2b12297a01fd7
ukon1990 <mail@jonaskf.net>
2019-01-02 18:44:42 +0100

    Fix in case invalid region is selected.
    Won't crash server. China won't work, due to regional limitations I think.

--- server/src/util/realm.util.ts
+++ server/src/util/realm.util.ts
@@ -4,8 +4,13 @@ import * as http from 'request';
 export class RealmUtil {
   static getRealmStatus(res, req) {
     http.get(new Endpoints()
-      .getPath(`realm/status?locale=en_GB`, req.query.region), (err, r, body) => {
-     res.send(JSON.parse(body));
-    });
+        .getPath(`realm/status?locale=en_GB`, req.params.region),
+      (err, r, body) => {
+        try {
+          res.send(JSON.parse(body));
+        } catch (error) {
+          res.send(body);
+        }
+      });
   }
 }
