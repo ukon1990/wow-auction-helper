3d0d34fadc2fb1784c3cb8ece784a3d5aaed99b0
Ukon1990 <mail@jonaskf.net>
2019-03-07 14:27:58 +0100

    updates to packages and avoiding error in realms upon init

---
+++ client/src/app/utils/object.util.ts
@@ -0,0 +1,80 @@
+import {Report} from './report.util';
+
+export class ObjectUtil {
+
+  public static isObject(value: any): boolean {
+    return value && typeof value === 'object' && value !== null && !value.forEach && !value.push;
+  }
+
+  public static isArray(value: any): boolean {
+    return value && typeof value === 'object' && value !== null && value.forEach && value.push;
+  }
+
+  public static isAPopulatedObject(value: any): boolean {
+    return value !== null && typeof value === 'object' && value.length >= 0;
+  }
+
+  public static overwrite(from: object, to: object): void {
+    if (!from || from === null) {
+      Report.debug(
+        {message: ''} as Error,
+        'Could not clone an object');
+      return;
+    }
+
+    Object.keys(from).forEach(key => {
+      if (ObjectUtil.isAPopulatedObject(from[key]) && to) {
+        to[key] = ObjectUtil.cloneArray(from[key]);
+      } else if (ObjectUtil.isObject(from[key]) && to) {
+        to[key] = ObjectUtil.overwrite(from[key], to[key]);
+      } else {
+        try {
+          to[key] = from[key];
+        } catch (e) {
+        }
+      }
+    });
+  }
+
+  public static clone(object: object): object {
+    if (!object || object === null) {
+      Report.debug(
+        {message: ''} as Error,
+        'Could not clone an object');
+      return object;
+    }
+
+    const obj = {};
+    Object.keys(object).forEach(key => {
+      if (ObjectUtil.isAPopulatedObject(object[key])) {
+        obj[key] = ObjectUtil.cloneArray(object[key]);
+      } else if (ObjectUtil.isObject(object[key])) {
+        obj[key] = ObjectUtil.clone(object[key]);
+      } else {
+        obj[key] = object[key];
+      }
+    });
+    return obj;
+  }
+
+  public static cloneArray(array: Array<any>): Array<any> {
+    if (!array || array === null) {
+      Report.debug(
+        {message: ''} as Error,
+        'Could not clone an array');
+      return array;
+    }
+
+    const newArray = Array<any>();
+    array.forEach(value => {
+      if (ObjectUtil.isAPopulatedObject(value)) {
+        newArray.push(ObjectUtil.cloneArray(value));
+      } else if (ObjectUtil.isObject(value)) {
+        newArray.push(ObjectUtil.clone(value));
+      } else {
+        newArray.push(value);
+      }
+    });
+    return newArray;
+  }
+}
