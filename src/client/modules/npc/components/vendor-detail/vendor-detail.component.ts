25575ec56b82a23322c70f75d33ad378de9c5472
Ukon1990 <mail@jonaskf.net>
2019-08-18 20:47:10 +0200

    .

--- src/client/modules/npc/components/vendor-detail/vendor-detail.component.ts
+++ src/client/modules/npc/components/vendor-detail/vendor-detail.component.ts
@@ -11,13 +11,15 @@ export class VendorDetailComponent implements OnInit {
   @Input() vendor: Vendor;
   columns: ColumnDescription[] = [
     {key: 'name', title: 'Name', dataType: 'name'},
-    {key: 'cost', title: 'Buy price', dataType: 'gold'},
+    {key: 'cost', title: 'Buy price', dataType: 'vendor-currency'},
     {key: 'profit', title: 'Profit', dataType: 'gold'},
     {key: 'stock', title: 'Stock', dataType: 'number'},
-    {key: 'currency', title: 'Currency', dataType: 'number'},
+    {key: 'avgDailySold', title: 'Daily sold', dataType: 'number', hideOnMobile: true},
+    {key: 'regionSaleRate', title: 'Sale rate', dataType: 'percent', hideOnMobile: true}
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
