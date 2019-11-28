24f82932cec09fbf486e8159f4f35961e07b5ed0
Ukon1990 <mail@jonaskf.net>
2019-11-28 14:44:46 +0100

    test

---
+++ src/lambda/handlers/item.handler.spec.ts
@@ -0,0 +1,23 @@
+import {environment} from '../../environments/environment';
+import {ItemHandler} from './item.handler';
+import {Item} from '../models/item/item';
+
+describe('ItemHandler', () => {
+  let originalEnvironment;
+  beforeAll(() => {
+    originalEnvironment = environment.test;
+    environment.test = true;
+  });
+  afterAll(() => {
+    environment.test = originalEnvironment;
+  });
+
+  describe('getById', () => {
+    it('It works if the id exists', async () => {
+      const id = 168154;
+      const result: Item = await new ItemHandler().getFromBlizzard(id, 'en_GB');
+      expect(result.id).toBe(id);
+    });
+
+  });
+});
