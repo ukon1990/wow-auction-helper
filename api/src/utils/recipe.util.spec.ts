89a7a5073a9ed89bfb83b758cea2e109f4934f03
Ukon1990 <mail@jonaskf.net>
2020-01-03 23:42:57 +0100

    New recipe list

--- api/src/utils/recipe.util.spec.ts
+++ api/src/utils/recipe.util.spec.ts
@@ -1,5 +1,16 @@
+import {RecipeUtil} from './recipe.util';
+
 describe('Test', () => {
-  it('', () => {
-    expect(1).toBe(1);
+  it('getRecipeListForPatch', async () => {
+    const list = await RecipeUtil.getRecipeListForPatch(80300);
+    expect(list.length).toBe(7);
+    expect(list[0].spellID).toBe(305936);
+    expect(list[0].itemID).toBe(170312);
+    expect(list[0].minCount).toBe(1);
+    expect(list[0].maxCount).toBe(1);
+    expect(list[1].name).toBe('Uncanny Combatant\'s Sorcerous Scepter');
+    expect(list[0].rank).toBe(1);
+    expect(list[1].rank).toBe(3);
+    expect(list[2].rank).toBe(2);
   });
 });
--- api/src/utils/recipe.util.ts
+++ api/src/utils/recipe.util.ts
@@ -1,5 +1,7 @@
 import {Recipe} from '../models/crafting/recipe';
 import {Reagent} from '../models/crafting/reagent';
+import {HttpClientUtil} from './http-client.util';
+import {WoWHeadUtil} from './wowhead.util';
 
 export class RecipeUtil {
   public static convert(wowDB): Recipe {
@@ -22,5 +24,89 @@ export class RecipeUtil {
       r.push({itemID: reagent.Item, name: '', count: reagent.ItemQty, dropped: false});
     });
     return r;
+  }
+
+  static getRecipeListForPatch(patchNumber: number): Promise<Recipe[]> {
+    const professions = [
+      {key: 'enchanting', name: 'Enchanting'},
+      {key: 'alchemy', name: 'Alchemy'},
+    ];
+    return new Promise<any>((resolve, reject) => {
+      Promise.all(professions.map(profession =>
+        this.getRecipeListForPatchAndProfession(patchNumber, profession)))
+        .then((lists) => {
+          let recipes = [];
+          lists.forEach(r => recipes = [...recipes, ...r]);
+          resolve(recipes);
+        })
+        .catch(reject);
+    });
+  }
+
+  static getRecipeListForPatchAndProfession(patchNumber: number, {key, name}: {key, name}): Promise<Recipe[]> {
+    return new Promise<Recipe[]>((resolve, reject) => {
+      new HttpClientUtil().get(`https://ptr.wowhead.com/${key}-spells?filter=21;2;${patchNumber}`, false)
+        .then(async ({body}) => {
+          const list = this.getSpellList(body);
+          resolve(await this.mapResultToRecipe(list, name));
+        })
+        .catch(reject);
+    });
+  }
+
+  private static async mapResultToRecipe(list, profession) {
+    const recipes = [];
+    for (const recipe of list) {
+      const {id, creates, name, reagents} = recipe;
+      const rank: number = await this.getRankForRecipe(id);
+      recipes.push({
+        spellID: id,
+        itemID: creates[0],
+        name,
+        minCount: creates[1],
+        maxCount: creates[2],
+        rank,
+        profession,
+        reagents: reagents.map(reagent => ({
+          itemID: reagent[0],
+          name: '',
+          count: reagent[1],
+          dropped: undefined
+        })),
+        expansion: 8
+      });
+    }
+    return recipes;
+  }
+
+  private static getSpellList(body: string) {
+    const regex = /var listviewspells = \[([\s\S]*?)];/gm,
+      result = regex.exec(body);
+    try {
+      if (result && result[0]) {
+        // tslint:disable-next-line:no-eval
+        return eval(result[0]
+          .replace(/var listviewspells =/g, '')
+          .replace(/;$/g, ''));
+      }
+    } catch (e) {
+
+    }
+    return [];
+  }
+
+  private static getRankForRecipe(id: number): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      new HttpClientUtil().get(`https://www.wowhead.com/tooltip/spell/${id}?locale=en`)
+        .then(({body}) => {
+          const regexResult = (/Rank [\d]{0,1}/g).exec(body.tooltip);
+          let rank;
+          if (regexResult && regexResult[0]) {
+            rank = +regexResult[0].replace(/Rank /g, '');
+          }
+          resolve(rank);
+        })
+        .catch(() => resolve(undefined));
+    });
   }
 }
