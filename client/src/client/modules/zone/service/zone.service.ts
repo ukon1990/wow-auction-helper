4900b6fd93d1c97083c34b96bad2484c58458124
Ukon1990 <mail@jonaskf.net>
2020-01-07 16:59:39 +0100

    Zone data is now stored on the client and only updated if needed.

--- client/src/client/modules/zone/service/zone.service.ts
+++ client/src/client/modules/zone/service/zone.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Endpoints} from '../../../services/endpoints';
 import {BehaviorSubject} from 'rxjs';
 import {Zone} from '../models/zone.model';
+import {DatabaseService} from '../../../services/database.service';
+import {ErrorReport} from '../../../utils/error-report.util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,30 +14,67 @@ export class ZoneService {
   list: BehaviorSubject<Zone[]> = new BehaviorSubject([]);
   mapped: BehaviorSubject<Map<number, Zone>> = new BehaviorSubject(new Map());
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private dbService: DatabaseService) {
   }
 
-  getAll(): Promise<any[]> {
+  getAll(): Promise<Zone[]> {
+    const locale = localStorage['locale'];
+    return new Promise<Zone[]>(async (resolve, reject) => {
+      await this.dbService.getAllZones()
+        .then(list => this.mapAndSetNextValueForZones(list))
+        .catch(console.error);
+
+      if (!this.list.value.length) {
+        await this.getAllFromS3();
+      }
+
+      await this.getAllAfterTimestamp();
+      resolve(this.list.value);
+    });
+  }
+
+  private getAllFromS3(): Promise<any[]> {
     const locale = localStorage['locale'];
     return new Promise<any[]>((resolve, reject) => {
       this.http.get(`${Endpoints.S3_BUCKET}/zone/${locale}.json.gz`)
         .toPromise()
         .then((response) => {
-          const list = response['zones'],
-            map = new Map();
           localStorage[this.storageName] = response['timestamp'];
-          list.forEach(zone =>
-            map.set(zone.id, zone));
-          this.mapped.next(map);
-          this.list.next(list);
-          resolve(list);
+          this.mapAndSetNextValueForZones(response['zones']);
+          resolve(this.list.value);
         })
-        .catch(console.error);
+        .catch(error => {
+          ErrorReport.sendHttpError(error);
+          resolve(this.list.value);
+        });
     });
-    // return this.http.post('http://localhost:3000/npc/all', {locale: 'en_GB'}).toPromise() as Promise<any[]>;
   }
 
-  getIds(ids: number[]) {
-    return this.http.post('http://localhost:3000/npc', {ids}).toPromise();
+  private async getAllAfterTimestamp() {
+    const locale = localStorage['locale'];
+    return new Promise<Zone[]>((resolve) => {
+
+      this.http.post(Endpoints.getLambdaUrl('zone'),
+        {locale, timestamp: localStorage.getItem(this.storageName)})
+        .toPromise()
+        .then(response => {
+          localStorage[this.storageName] = response['timestamp'];
+          this.mapAndSetNextValueForZones(response['zones']);
+          resolve(this.list.value);
+        }).catch(error => {
+        ErrorReport.sendHttpError(error);
+        resolve(this.list.value);
+      });
+    });
+  }
+
+  private mapAndSetNextValueForZones(newData: Zone[]) {
+    const list = [...newData, ...this.list.value],
+      map = new Map();
+    list.forEach(zone =>
+      map.set(zone.id, zone));
+    this.mapped.next(map);
+    this.list.next(list);
+    return list;
   }
 }
