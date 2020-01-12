90a659bbf77bf0fb8437b9fee9dda9a4479b9400
Ukon1990 <mail@jonaskf.net>
2020-01-12 18:06:01 +0100

    We don't need to wait for the response
    When we are updating a realm, we don't care about the response from the target in the initiator (updateAllHouses) function.

--- api/src/utils/currency.util.spec.ts
+++ api/src/utils/currency.util.spec.ts
@@ -3,7 +3,6 @@ import {CurrencyUtil} from './currency.util';
 describe('CurrencyUtil', () => {
   it('getList', async () => {
     const list = await CurrencyUtil.getList();
-    console.log(list);
     expect(list.length).toBe(122);
   });
 });
