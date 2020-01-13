a0d30d761c78c87ed4ee8d76ebe2051ef879e642
Ukon1990 <mail@jonaskf.net>
2020-01-13 18:41:14 +0100

    Gzip decompress

--- api/src/utils/gzip.util.ts
+++ api/src/utils/gzip.util.ts
@@ -3,7 +3,6 @@ const zlib = require('zlib');
 export class GzipUtil {
   compress(input: any): Promise<Buffer> {
     return new Promise<Buffer>((resolve, reject) => {
-      const gzip = zlib.createGzip();
       zlib.gzip(JSON.stringify(input),
         (error: Error | null, buffer: Buffer) => {
           if (error) {
@@ -17,11 +16,11 @@ export class GzipUtil {
 
   decompress(input: Buffer): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      zlib.unzip(input, (error, data) => {
+      zlib.gunzip(input, (error, data) => {
         if (error) {
           reject(error);
         } else {
-          resolve(data);
+          resolve(JSON.parse(data.toString()));
         }
       });
     });
