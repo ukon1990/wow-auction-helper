ee88220dc33d6e069087443137652dff58ffb9ea
Ukon1990 <mail@jonaskf.net>
2020-06-07 23:18:45 +0200

    Update all recipes

--- api/src/utils/recipe/recipev2.util.spec.ts
+++ api/src/utils/recipe/recipev2.util.spec.ts
@@ -25,66 +25,83 @@ const setKeyMap = (res: any, keyMap: {} = {}) => {
 };
 
 describe('Recipev2Util', () => {
-    xit('Can get recipe', async () => {
-        const recipe: Recipev2 = await RecipeV2Util.getRecipeFromAPI(39416),
-            recipeMapped: Recipe = RecipeV2Util.mapToRecipe(recipe);
-        await RecipeV2Util.addToDB(39416, new DatabaseUtil())
-            .catch(() => {
-            });
-        expect(recipe.id).toBe(1631);
-        expect(recipeMapped.icon).toBe('asd');
-    });
-
-    xit('"bruteforce" recipes', async () => {
-        jest.setTimeout(10000000);
-        const conn = new DatabaseUtil(false),
-            increment = 200000,
-            start = 1353,
-            end = 50000; // start + increment;
-        let completed = 0, found = 0, lastId = 0;
-
-        for (let i = start; i < end; i++) {
-            await RecipeV2Util.addToDB(i, conn)
-                .then((recipe) => {
-                    found++;
-                    lastId = recipe.id;
-                })
+    describe('Not really tests', () => {
+        xit('Can get recipe', async () => {
+            const recipe: Recipev2 = await RecipeV2Util.getRecipeFromAPI(39416),
+                recipeMapped: Recipe = RecipeV2Util.mapToRecipe(recipe);
+            await RecipeV2Util.addToDB(39416, new DatabaseUtil())
                 .catch(() => {
                 });
-            completed++;
-            console.log('Procesed', completed, '/', end - start,
-                `(${((completed / (end - start)) * 100).toFixed(2)}%) id=${i
-                } found=${found
-                } last id was=${lastId
-                }`);
-        }
-        conn.end();
+            expect(recipe.id).toBe(1631);
+            expect(recipeMapped.icon).toBe('asd');
+        });
 
-        expect(completed).toBe(increment);
-    });
+        xit('"bruteforce" recipes', async () => {
+            jest.setTimeout(10000000);
+            const conn = new DatabaseUtil(false),
+                increment = 200000,
+                start = 1353,
+                end = 50000; // start + increment;
+            let completed = 0, found = 0, lastId = 0;
 
-    xit('map all recipe keys', async () => {
-        const recipes: Recipev2[] = [],
-            keyMap = {};
-        await new DatabaseUtil().query(`
+            for (let i = start; i < end; i++) {
+                await RecipeV2Util.addToDB(i, conn)
+                    .then((recipe) => {
+                        found++;
+                        lastId = recipe.id;
+                    })
+                    .catch(() => {
+                    });
+                completed++;
+                console.log('Procesed', completed, '/', end - start,
+                    `(${((completed / (end - start)) * 100).toFixed(2)}%) id=${i
+                    } found=${found
+                    } last id was=${lastId
+                    }`);
+            }
+            conn.end();
+
+            expect(completed).toBe(increment);
+        });
+
+        it('Updating existing', async () => {
+            jest.setTimeout(9999999);
+            const db = new DatabaseUtil(false);
+            const ids = await db.query('SELECT id FROM recipe;');
+            let completed = 0;
+            for (const {id} of ids) {
+                await RecipeV2Util.update(id, db)
+                    .catch(console.error);
+                completed++;
+                console.log(`Progrss ${Math.round(completed / ids.length * 100)}%`, completed, ids.length);
+            }
+            db.end();
+            expect(completed).toBe(ids.length);
+        });
+
+        xit('map all recipe keys', async () => {
+            const recipes: Recipev2[] = [],
+                keyMap = {};
+            await new DatabaseUtil().query(`
             SELECT data
-            FROM wah.recipe_raw;`)
-            .then(r => {
-                r.forEach(recipe => {
-                    const res: Recipev2 = JSON.parse(recipe.data);
-                    recipes.push(res);
-                    setKeyMap(res, keyMap);
-                });
-            })
-            .catch(console.error);
-        console.log('Map is', JSON.stringify(keyMap));
-        expect(recipes.length).toBe(7210);
-    });
+            FROM wah.recipe;`)
+                .then(r => {
+                    r.forEach(recipe => {
+                        const res: Recipev2 = JSON.parse(recipe.data);
+                        recipes.push(res);
+                        setKeyMap(res, keyMap);
+                    });
+                })
+                .catch(console.error);
+            console.log('Map is', JSON.stringify(keyMap));
+            expect(recipes.length).toBe(7210);
+        });
 
 
-    it('getAndMapProfessions',  async() => {
-        const mapped = await RecipeV2Util.getAndMapProfessions();
-        console.log('mapped', mapped);
-        expect(mapped).toBeTruthy();
+        it('getAndMapProfessions',  async() => {
+            const mapped = await RecipeV2Util.getAndMapProfessions();
+            console.log('mapped', mapped);
+            expect(mapped).toBeTruthy();
+        });
     });
 });
--- api/src/utils/recipe/recipev2.util.ts
+++ api/src/utils/recipe/recipev2.util.ts
@@ -7,54 +7,71 @@ import {DatabaseUtil} from '../database.util';
 
 export class RecipeV2Util {
 
-  static getRecipeFromAPI(id: number): Promise<Recipev2> {
-    return new Promise(async (resolve, reject) => {
-      await AuthHandler.getToken();
-      // TODO: Fetch media
-      const url = new Endpoints().getPath(`recipe/${id}`, 'eu', 'static');
-      new HttpClientUtil().get(url)
-        .then(({body}) => resolve(body))
-        .catch(reject);
-    });
-  }
+    static getRecipeFromAPI(id: number): Promise<Recipev2> {
+        return new Promise(async (resolve, reject) => {
+            await AuthHandler.getToken();
+            // TODO: Fetch media
+            const url = new Endpoints().getPath(`recipe/${id}`, 'eu', 'static');
+            new HttpClientUtil().get(url)
+                .then(({body}) => resolve(body))
+                .catch(reject);
+        });
+    }
 
-  static async addToDB(id: number, conn: DatabaseUtil = new DatabaseUtil()): Promise<Recipev2> {
+    static async addToDB(id: number, conn: DatabaseUtil = new DatabaseUtil()): Promise<Recipev2> {
 
-    return new Promise(async (resolve, reject) => {
-      RecipeV2Util.getRecipeFromAPI(id)
-        .then(async recipe => {
-          const sql = format(`
+        return new Promise(async (resolve, reject) => {
+            RecipeV2Util.getRecipeFromAPI(id)
+                .then(async recipe => {
+                    const sql = format(`
               INSERT INTO \`wah\`.\`recipe\`
               (\`id\`,
                \`data\`)
               VALUES (?, ?);`, [recipe.id, JSON.stringify(recipe)]);
-          await conn.query(sql)
-            .then(() => resolve(recipe))
-            .catch(reject);
-        })
-        .catch(reject);
-    });
-  }
+                    await conn.query(sql)
+                        .then(() => resolve(recipe))
+                        .catch(reject);
+                })
+                .catch(reject);
+        });
+    }
 
-  static async getAndMapProfessions() {
-      return new Promise(async (resolve, reject) => {
-          await AuthHandler.getToken();
-          const url = new Endpoints().getPath('profession/index', 'eu', 'static');
-          const http = new HttpClientUtil();
-          const recipeMap = {};
-          try {
-              const {body: professions} = await http.get(url);
-              console.log(url)
-              resolve(professions);
-          } catch (e) {
-              console.error(e);
-              reject(e);
-          }
-      });
-  }
+    static async getAndMapProfessions() {
+        return new Promise(async (resolve, reject) => {
+            await AuthHandler.getToken();
+            const url = new Endpoints().getPath('profession/index', 'eu', 'static');
+            const http = new HttpClientUtil();
+            const recipeMap = {};
+            try {
+                const {body: professions} = await http.get(url);
+                console.log(url)
+                resolve(professions);
+            } catch (e) {
+                console.error(e);
+                reject(e);
+            }
+        });
+    }
 
-  static mapToRecipe(recipe: Recipev2) {
-    return undefined;
-  }
+    static mapToRecipe(recipe: Recipev2) {
+        return undefined;
+    }
+
+    static update(id: any, conn: DatabaseUtil) {
+        return new Promise((resolve, reject) => {
+            RecipeV2Util.getRecipeFromAPI(id)
+                .then(async recipe => {
+                    const sql = format(`
+                                        UPDATE recipe
+                                        SET data = ?
+                                        WHERE id = ?;`,
+                        [JSON.stringify(recipe), recipe.id]);
+                    conn.query(sql)
+                        .then(() => resolve(recipe))
+                        .catch(reject);
+                })
+                .catch(reject);
+        });
+    }
 }
 
