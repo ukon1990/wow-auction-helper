15bf9b305285ccff0e0e612aac49b4680ef559dc
Ukon1990 <mail@jonaskf.net>
2020-07-06 11:49:55 +0200

    Some chart changes and color changes

--- client/src/client/modules/dashboard/components/ah-summary/summary-card/summary-card.component.ts
+++ client/src/client/modules/dashboard/components/ah-summary/summary-card/summary-card.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {Report} from '../../../../../utils/report.util';
 import {ColumnDescription} from '../../../../table/models/column-description';
 import {SummaryCard} from '../../../../../models/summary-card.model';
-import {ChartData} from '../../../../../models/chart-data.model';
+import {NumberUtil} from '../../../../util/utils/number.util';
+import {GoldPipe} from '../../../../util/pipes/gold.pipe';
+import {ChartData} from '../../../../util/models/chart.model';
+import {ChartData as SummaryCardData} from '../../../../../models/chart-data.model';
 
 @Component({
   selector: 'wah-summary-card',
@@ -10,7 +12,7 @@ import {ChartData} from '../../../../../models/chart-data.model';
   styleUrls: ['./summary-card.component.scss']
 })
 export class SummaryCardComponent implements OnInit, OnChanges {
-  @Input() summary: any;
+  @Input() summary: SummaryCard;
   @Input() useChart: boolean;
 
   columns: ColumnDescription[] = [
@@ -18,6 +20,15 @@ export class SummaryCardComponent implements OnInit, OnChanges {
     {key: 'value', title: 'Quantity', dataType: 'number'}
   ];
   tableData = [];
+  dateData: ChartData = {
+    labels: [],
+    datasets: [],
+    labelCallback: this.tooltipCallback
+  };
+  private axisLabels = {
+    yAxis1: 'Quantity',
+    xAxis: 'Source'
+  };
 
   constructor() {
   }
@@ -29,19 +40,57 @@ export class SummaryCardComponent implements OnInit, OnChanges {
     if (changes.summary && changes.summary.currentValue) {
       const summary: SummaryCard = changes.summary.currentValue;
       this.tableData.length = 0;
+      this.resetData();
       summary.data
-        .forEach((data: ChartData) =>
+        .forEach((data: SummaryCardData) => {
           this.tableData.push({
-            id: summary.labels[data.id] ? summary.labels[data.id].value : data.id,
+            id: this.getLabel(summary, data),
             value: data.value
-          }));
+          });
+
+          this.setChartData(summary, data);
+        });
 
       this.tableData
-        .sort((a: ChartData, b: ChartData) =>
+        .sort((a: SummaryCardData, b: SummaryCardData) =>
           b.value - a.value);
     }
   }
 
+  private getLabel(summary: SummaryCard, data: SummaryCardData) {
+    return summary.labels[data.id] ? summary.labels[data.id].value : data.id;
+  }
+
   selection(id: number) {
+  }
+
+  resetData() {
+    this.dateData.labels.length = 0;
+    this.dateData.datasets = [];
+    this.dateData.axisLabels = this.axisLabels;
+    this.dateData.datasets.push({
+      label: this.summary.title,
+      data: [],
+      type: 'bar',
+      fill: 2,
+      yAxisID: 'yAxes-1',
+      backgroundColor: 'rgba(0, 255, 22, 0.4)'
+    });
+  }
+
+  tooltipCallback(items, data): string {
+    const {index, datasetIndex} = items;
+    const dataset = data.datasets[datasetIndex];
+    if (datasetIndex > 2) {
+      return dataset.label + ': ' +
+        NumberUtil.format(dataset.data[index]);
+    }
+    return dataset.label + ': ' +
+      new GoldPipe().transform(dataset.data[index] * 10000);
+  }
+
+  private setChartData(summary: SummaryCard, data: SummaryCardData) {
+    this.dateData.labels.push(this.getLabel(summary, data));
+    this.dateData.datasets[0].data.push(data.value);
   }
 }
