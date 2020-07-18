3cb462b0fbda7914e20a1e8eaa0e90c6a4c9bd1b
Ukon1990 <mail@jonaskf.net>
2020-07-18 23:47:50 +0200

    Started on single dashboard calculation

--- client/src/client/modules/dashboard/utils/dashboard-calculate.util.spec.ts
+++ client/src/client/modules/dashboard/utils/dashboard-calculate.util.spec.ts
@@ -3,6 +3,8 @@ import {ItemRule, Rule} from '../models/rule.model';
 import {ConditionEnum} from '../types/condition.enum';
 import {TargetValueEnum} from '../types/target-value.enum';
 import {DashboardCalculateUtil} from './dashboard-calculate.util';
+import {Recipe} from '../../crafting/models/recipe';
+import {AuctionItem} from '../../auction/models/auction-item.model';
 
 const getBoard = (rules: Rule[] = [], itemRules?: ItemRule[]) => ({
   id: 1,
@@ -16,16 +18,30 @@ const getBoard = (rules: Rule[] = [], itemRules?: ItemRule[]) => ({
 } as DashboardV2);
 
 fdescribe('DashboardCalculateUtil', () => {
+  const firstItem = new AuctionItem(1);
+
+  const secondItem = new AuctionItem(2);
+
+  const auctionItems: AuctionItem[] = [
+    firstItem, secondItem
+  ];
+  const recipeMap = new Map<number, Recipe>();
+
+  beforeAll(() => {
+    // recipeMap.set(32, []);
+  });
+
   describe('calculate', () => {
     it('With item rules', () => {
-      const board: DashboardV2 = DashboardCalculateUtil.calculate(getBoard([
-        {
-          condition: ConditionEnum.EQUAL_TO,
-          targetValueType: TargetValueEnum.NUMBER,
-          field: '',
-          toField: '',
-        }
-      ]));
+      const board: DashboardV2 = DashboardCalculateUtil.calculate(
+        getBoard([
+          {
+            condition: ConditionEnum.EQUAL_TO,
+            targetValueType: TargetValueEnum.NUMBER,
+            field: '',
+            toField: '',
+          }
+        ]), auctionItems, recipeMap);
 
       expect(board.data.length).toBe(1);
     });
--- client/src/client/modules/dashboard/utils/dashboard-calculate.util.ts
+++ client/src/client/modules/dashboard/utils/dashboard-calculate.util.ts
@@ -1,7 +1,10 @@
 import {DashboardV2} from '../models/dashboard-v2.model';
+import {AuctionItem} from '../../auction/models/auction-item.model';
+import {Recipe} from '../../crafting/models/recipe';
 
 export class DashboardCalculateUtil {
-  static calculate(board: DashboardV2): DashboardV2 {
+  static calculate(board: DashboardV2, items: AuctionItem[], knownRecipeItemMap: Map<number, Recipe>): DashboardV2 {
+
     return board;
   }
 }
