cecea082e20a217e1187b6803a844e833e8c567f
Ukon <mail@jonaskf.net>
2020-11-28 14:39:01 +0100

    start

---
+++ api/src/recipe/modified-crafting.util.ts
@@ -0,0 +1,7 @@
+/*
+TODO: Map auction stats on modifier as well!
+- Create a general mapper for modifiers
+- Add new DB key for modifiers
+ */
+
+// https://us.api.blizzard.com/data/wow/modified-crafting
