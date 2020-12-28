6ab8c02e7c2460716515b53efbdcbcda5dd843d3
Ukon <mail@jonaskf.net>
2020-12-28 16:32:53 +0100

    Get start of day for timeszone in CET

--- api/src/auction/services/stats.service.spec.ts
+++ api/src/auction/services/stats.service.spec.ts
@@ -1,7 +1,6 @@
 import {environment} from '../../../../client/src/environments/environment';
 import {StatsService} from './stats.service';
 import {DatabaseUtil} from '../../utils/database.util';
-import {StatsRepository} from '../repository/stats.repository';
 import {AuctionStatsUtil} from '../utils/auction-stats.util';
 import {ItemStats} from '../models/item-stats.model';
 import {AuctionProcessorUtil} from '../utils/auction-processor.util';
@@ -57,6 +56,78 @@ describe('StatsService', () => {
     it('The trend quantity should be identical', () => {
       expect(toStatic.past24Hours.quantity.trend)
         .toBe(toClient.past24Hours.quantity.trend);
+    });
+  });
+
+  describe('getStartOfTodayForTimeZone', () => {
+    const exampleDate = new Date('2020-12-24T15:30:00.000Z');
+
+    const getLocalTime = (date: Date, timezone: string): string => Intl.DateTimeFormat([], {
+      hour: 'numeric',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      timeZone: timezone
+    }).format(date);
+
+    it('America/Los_Angeles', () => {
+      const tz = 'America/Los_Angeles';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-24T08:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+
+    it('America/Denver', () => {
+      const tz = 'America/Denver';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-24T07:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+
+    it('America/Chicago', () => {
+      const tz = 'America/Chicago';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-24T06:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+
+    it('America/New_York', () => {
+      const tz = 'America/New_York';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-24T05:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+
+    it('America/Sao_Paulo', () => {
+      const tz = 'America/Sao_Paulo';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-24T03:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+
+    it('Europe/Paris', () => {
+      const tz = 'Europe/Paris';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-23T23:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+    it('Asia/Taipei', () => {
+      const tz = 'Asia/Taipei';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-23T16:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+    it('Asia/Seoul', () => {
+      const tz = 'Asia/Seoul';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-23T15:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
+    });
+    it('Australia/Melbourne', () => {
+      const tz = 'Australia/Melbourne';
+      const res: Date = new StatsService().getStartOfTodayForTimeZone(tz, exampleDate);
+      expect(res).toEqual(new Date('2020-12-23T13:00:00.000Z'));
+      expect(getLocalTime(res, tz)).toEqual('12/24/2020, 12 AM');
     });
   });
 });
--- api/src/auction/services/stats.service.ts
+++ api/src/auction/services/stats.service.ts
@@ -16,11 +16,40 @@ import {ItemDailyPriceEntry, ItemPriceEntry} from '../../../../client/src/client
 const request: any = require('request');
 const PromiseThrottle: any = require('promise-throttle');
 
+enum TimeZone {
+  'America/Los_Angeles' = -8,
+  'America/Denver' = -7,
+  'America/Chicago' = -6,
+  'America/New_York' = -5,
+  'America/Sao_Paulo' = -3,
+  'Europe/Paris' = 1,
+  'Asia/Taipei' = 8,
+  'Asia/Seoul' = 9,
+  'Australia/Melbourne' = 11,
+}
+
 export class StatsService {
   realmRepository: RealmRepository;
 
   constructor() {
     this.realmRepository = new RealmRepository();
+  }
+
+  /**
+   * Calculating at what time 00:00 was in the given timezone today
+   * @param timezone
+   * @param date
+   */
+  getStartOfTodayForTimeZone(timezone: string, date = new Date()): Date {
+    const offsetHours = +TimeZone[timezone];
+
+    date.setUTCHours(0);
+    date.setUTCMinutes(0);
+    date.setUTCSeconds(0);
+    date.setUTCMilliseconds(0);
+
+    console.log('Date', date, new Date(+date - offsetHours * 60 * 60 * 1000), offsetHours);
+    return new Date(+date - offsetHours * 60 * 60 * 1000);
   }
 
   /* istanbul ignore next */
