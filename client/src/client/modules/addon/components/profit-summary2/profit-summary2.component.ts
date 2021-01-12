ee199e1f3c4d974900827f4f68ba68f60003d5d7
Ukon <mail@jonaskf.net>
2021-01-12 18:44:20 +0100

    column and data model start

--- client/src/client/modules/addon/components/profit-summary2/profit-summary2.component.ts
+++ client/src/client/modules/addon/components/profit-summary2/profit-summary2.component.ts
@@ -3,6 +3,23 @@ import {FormControl, FormGroup} from '@angular/forms';
 import {TSMCSV, TsmLuaUtil} from '../../../../utils/tsm/tsm-lua.util';
 import {SubscriptionManager} from '@ukon1990/subscription-manager';
 import {ColumnDescription} from '../../../table/models/column-description';
+import {SharedService} from '../../../../services/shared.service';
+
+interface TableEntry {
+  id: number;
+  petSpeciesId: number;
+  bonusIds: number[];
+  uniqueId: string;
+  name: string;
+  soldValue: number;
+  soldQuantity: number;
+  buyValue: number;
+  buyQuantity: number;
+}
+
+interface TableData {
+  [key: string]: TableEntry[];
+}
 
 @Component({
   selector: 'wah-profit-summary2',
@@ -16,30 +33,45 @@ export class ProfitSummary2Component implements OnInit, OnDestroy {
     startDate: new FormControl(),
     endDate: new FormControl(),
   });
-  columns: ColumnDescription = [];
+  columns: ColumnDescription[] = [
+    {key: 'name', title: 'Name', dataType: 'name'},
+  ];
+  tableData: TableData;
   private sm = new SubscriptionManager();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.sm.add(
       TsmLuaUtil.events,
       (data: TSMCSV) =>
         this.handleTsmEvent(data));
+
+    this.initContent();
   }
 
   ngOnDestroy(): void {
     this.sm.unsubscribe();
   }
 
+  initContent(): void {
+    const realm = SharedService.realms[SharedService.user.realm];
+    if (realm) {
+      this.form.controls.realm.setValue(realm.name);
+    } else {
+      this.sm.add(
+        SharedService.events.realms,
+        () => this.initContent(),
+        {terminateUponEvent: true});
+    }
+  }
 
   handleTsmEvent(data: TSMCSV): void {
     if (!data) {
       return;
     }
     this.realms.length = 0;
-
-    // this.profitSummary = data.profitSummary;
 
     Object.keys(data.profitSummary)
       .forEach(realm =>
