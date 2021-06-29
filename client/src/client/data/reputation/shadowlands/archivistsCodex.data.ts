d5334bf1d444c7f94058734e8b64a505d7d179d2
Jonas <services@jonaskf.net>
2021-06-29 06:30:50 +0200

    rep

---
+++ client/src/client/data/reputation/shadowlands/archivistsCodex.data.ts
@@ -0,0 +1,37 @@
+import {ReputationVendor, ReputationVendorRecipe, Vendor} from '../../../models/reputation.model';
+import {Standing} from '../standings.data';
+
+export const archivistsCodex: ReputationVendor = {
+  id: 2472,
+  name: `Death's advance`,
+  vendors: [// https://www.wowhead.com/guides/archivists-codex-reputation-guide
+    new Vendor('Archivist Roh-Suir', true, true, [{x: 62.8, y: 22.6}], 178257),
+  ],
+  expansion: 8, // Tome of origins is for all professions
+  professions: {
+    164: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+    165: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+    171: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+      new ReputationVendorRecipe(1, 'Transmute: Stones to Ore', 1, [2000, 0, 1931], '4'),
+    ],
+    197: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+    202: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+    755: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+    773: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark of the Chained Isle', 1, [2500, 0, 1931], '4'),
+    ],
+  },
+  isAlly: true,
+  isHorde: true,
+};
---
+++ client/src/client/data/reputation/shadowlands/deathsAdvance.data.ts
@@ -0,0 +1,40 @@
+import {ReputationVendor, ReputationVendorRecipe, Vendor} from '../../../models/reputation.model';
+import {Standing} from '../standings.data';
+
+export const deathsAdvance: ReputationVendor = {
+  id: 2413,
+  name: `Death's advance`,
+  vendors: [// https://www.wowhead.com/guides/deaths-advance-reputation-guide
+    new Vendor('Duchess Mynx', true, true, [{x: 63.4, y: 23.4}], 179321),
+  ],
+  expansion: 8, // Tome of origins is for all professions
+  professions: {
+    164: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark III', 1, [2000, 0, 1767], Standing.Honored),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+    165: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark III', 1, [2000, 0, 1767], Standing.Honored),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+    197: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark III', 1, [2000, 0, 1767], Standing.Honored),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+    202: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark III', 1, [2000, 0, 1767], Standing.Honored),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+    755: [
+      new ReputationVendorRecipe(1, 'Recipe: Crafter\'s Mark III', 1, [2000, 0, 1767], Standing.Honored),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+    773: [// item id = 186598
+      // Item id = 186724
+      new ReputationVendorRecipe(1, 'Technique: Contract: Death\'s Advance', 1, [2500, 0, 1767], Standing.Revered),
+      new ReputationVendorRecipe(1, 'Tome of Origins', 1, [2000, 0, 1767], Standing.Honored),
+    ],
+  },
+  isAlly: true,
+  isHorde: true,
+};
