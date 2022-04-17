f36dc446f6f5baa465634146a2061795955d9d91
Jonas <services@jonaskf.net>
2022-04-17 22:48:30 +0200

    nope

--- api-3/src/functions/hello/handler.ts
+++ api-3/src/functions/hello/handler.ts
@@ -1,12 +1,14 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
+import type {ValidatedEventAPIGatewayProxyEvent} from '@libs/api-gateway';
+import {formatJSONResponse} from '@libs/api-gateway';
+import {middyfy} from '@libs/lambda';
 
 import schema from './schema';
+import {NumberUtil} from "@shared/utils";
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const x = NumberUtil.format(1);
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    message: `Hello ${event.body.name + x}, welcome to the exciting Serverless world!`,
     event,
   });
 };
