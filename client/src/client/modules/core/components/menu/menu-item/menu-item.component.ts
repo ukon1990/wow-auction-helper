573f717cba116b9c5442adce65ba3d73355993f8
Jonas <services@jonaskf.net>
2022-12-10 12:04:20 +0100

    Menu colors

--- client/src/client/modules/core/components/menu/menu-item/menu-item.component.ts
+++ client/src/client/modules/core/components/menu/menu-item/menu-item.component.ts
@@ -3,7 +3,12 @@ import {MenuItem} from '../../../models/menu-item.model';
 
 @Component({
   selector: 'wah-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  styles: [`
+    :not(.mat-accent) .button-color {
+        color: white;
+    }
+  `]
 })
 export class MenuItemComponent {
   @Input() item: MenuItem;
